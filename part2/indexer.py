1ce52157f7545c5e7998db366432b46fe3ce659a
yixin <yixin4@hotmail.com>
2020-03-06 18:26:17 -0500

    changed to a function

--- part2/indexer.py
+++ part2/indexer.py
@@ -4,27 +4,31 @@
 
 # import sys
 
-d = {}
 
-with open('./movies.dat', 'r') as f:
-    for line in f:
-        # Processes each line of the file
-        movie_id, movie_name, movie_genres = line.split('::', 2)
+def inverted_index(path):
+    d = {}
 
-        # Strip blank spaces
-        movie_id = movie_id.strip()
-        movie_name = movie_name.strip()
-        movie_genres = movie_genres.strip()
+    with open(path, 'r') as f:
+        for line in f:
+            # Processes each line of the file
+            movie_id, movie_name, movie_genres = line.split('::', 2)
 
-        # Processes the genres
-        movie_genres = movie_genres.split('|')
-        for genre in movie_genres:
-            # If genre is blank, replace it with 'None'
-            if genre == '':
-                genre = 'None'
-            # If genre is not yet in dictionary, add it
-            if genre not in d:
-                d[genre] = set()
-            d[genre].add(movie_name)
+            # Strip blank spaces
+            movie_id = movie_id.strip()
+            movie_name = movie_name.strip()
+            movie_genres = movie_genres.strip()
 
-print(d)
+            # Processes the genres
+            movie_genres = movie_genres.split('|')
+            for genre in movie_genres:
+                # If genre is blank, replace it with 'None'
+                if genre == '':
+                    genre = 'None'
+                # Convert genre to upper
+                genre = genre.upper()
+                # If genre is not yet in dictionary, add it
+                if genre not in d:
+                    d[genre] = set()
+                d[genre].add(movie_name)
+
+    return d
