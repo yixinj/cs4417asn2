1114d11066cdd63430f66ad41b8c823f5fd93cfe
yixin <yixin4@hotmail.com>
2020-03-05 20:26:55 -0500

    .

--- part1/query.py
+++ part1/query.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# take as input the name of a city and return the number of Starbucks counted in the output file when run in a bash terminal with 'python query.py' used to start the program.
+# take as input the name of a city and
+# return the number of Starbucks counted in the output file
+# when run in a bash terminal with 'python query.py' used to start the program.
 import sys
 
 d = {}
