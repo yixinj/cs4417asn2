74cc07325ce11872936103fdf789d0d68d818ffd
yixin <yixin4@hotmail.com>
2020-03-05 19:27:23 -0500

    output taken from hadoop

--- query.py
+++ query.py
@@ -1,9 +1,7 @@
 # take as input the name of a city and return the number of Starbucks counted in the output file when run in a bash terminal with 'python query.py' used to start the program.
 
-# import pandas as pd
-
-# df = pd.read_csv('starbucks-locations-sort.csv')
-# print(df.head())
-
 word, count = ['city', '1']
 print(word)
+
+with open('cityInformation', 'rb') as f:
+    print f
