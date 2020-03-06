e1785ec5f48c4b5946d1f6f259369cac57c09c73
yixin <yixin4@hotmail.com>
2020-03-05 19:34:51 -0500

    implemented query

--- query.py
+++ query.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+
 # take as input the name of a city and return the number of Starbucks counted in the output file when run in a bash terminal with 'python query.py' used to start the program.
+import sys
 
-word, count = ['city', '1']
-print(word)
-
+d = {}
 with open('cityInformation', 'rb') as f:
-    print f
+    for line in f:
+        line = line.strip()
+        word, count = line.split('\t')
+        d[word] = count
+
+for query in sys.stdin:
+    query = query.strip().upper()
+    print d[query]
