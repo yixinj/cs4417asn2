4a9569172c2666a16e5c05858b9f99d2d0cab9c8
yixin <yixin4@hotmail.com>
2020-03-06 20:41:10 -0500

    removed dictionary creation

--- part1/query.py
+++ part1/query.py
@@ -5,13 +5,11 @@
 # when run in a bash terminal with 'python query.py' used to start the program.
 import sys
 
-d = {}
-with open('cityInformation', 'r') as f:
-    for line in f:
-        line = line.strip()
-        word, count = line.split('\t')
-        d[word] = count
-
 for query in sys.stdin:
     query = query.strip().upper()
-    print d[query]
+    with open('part1/cityInformation', 'r') as f:
+        for line in f:
+            line = line.strip()
+            word, count = line.split('\t')
+            if query == word:
+                print count
