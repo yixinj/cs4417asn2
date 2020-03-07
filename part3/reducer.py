fbc0306c19b3d70b14025518e1afc03ed79db3dd
yixin <yixin4@hotmail.com>
2020-03-06 19:15:21 -0500

    implemented reducer

---
+++ part3/reducer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+
+current_genre = None
+current_movies = 0
+genre = None
+
+for line in sys.stdin:
+    # stdin: each line contains a genre (UPPER), tab, and movie name
+    line = line.strip()
+    genre, movie_name = line.split('\t', 1)
+
+    # Add the counts (works because Hadoop sorts entries)
+    if current_genre == genre:
+        current_movies += movie_name + ","
+    else:
+        if current_genre:
+            # Write result to stdout
+            print('%s\t%s' % (current_genre, current_movies))
+        current_movies = movie_name
+        current_genre = genre
+
+# Output last word if needed
+if current_genre == genre:
+    print('%s\t%s' % (current_genre, current_movies))
+
+# with open('output.txt', 'rb') as f:
